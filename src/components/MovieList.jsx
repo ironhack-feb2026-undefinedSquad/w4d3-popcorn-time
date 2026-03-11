939e833fc94e8512221498c90ded01de8a920367
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-03-11 12:53:25 +0100

    feat: implement functionality to delete movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,46 @@
+import { useState } from "react"
+
 import movies from "../data/movies.json"
 
 
 function MovieList() {
 
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies)
+
+
+    const deleteMovie = (movieId) => {
+        // get the new list of movies...
+        const newList = moviesToDisplay.filter((movieDetails, i, arr) => {
+            if (movieDetails.id !== movieId) {
+                return true
+            } else {
+                return false
+            }
+        })
+
+        // update state...
+        // moviesToDisplay = newList // NEVER, NEVER update state directly
+        setMoviesToDisplay(newList)
+    }
+
+
     return (
         <>
-            <h2>List of movies</h2>
+            <h2>Number of movies: {moviesToDisplay.length}</h2>
 
-            {movies.map((movieObj, i, arr) => {
+            {moviesToDisplay.map((movieObj, i, arr) => {
                 return (
                     <section key={movieObj.id} className="card">
                         <h3>{movieObj.title}</h3>
-                        
-                        { movieObj.imgURL 
+
+                        {movieObj.imgURL
                             && <img src={movieObj.imgURL} alt="Movie poster" />
                         }
 
                         <p>Year: {movieObj.year}</p>
                         <p>Rating: {movieObj.rating}</p>
+
+                        <button onClick={() => { deleteMovie(movieObj.id) }}>Delete this movie</button>
                     </section>
                 )
             })}
