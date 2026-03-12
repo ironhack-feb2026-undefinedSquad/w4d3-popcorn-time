ca9e317d6125b5550ea23f997f4e125f049047bd
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-03-12 10:42:01 +0100

    refactor: extract movie details to a separate component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 import movies from "../data/movies.json"
+import MovieSummary from "./MovieSummary"
 
 
 function MovieList() {
@@ -30,18 +31,10 @@ function MovieList() {
 
             {moviesToDisplay.map((movieObj, i, arr) => {
                 return (
-                    <section key={movieObj.id} className="card">
-                        <h3>{movieObj.title}</h3>
-
-                        {movieObj.imgURL
-                            && <img src={movieObj.imgURL} alt="Movie poster" />
-                        }
-
-                        <p>Year: {movieObj.year}</p>
-                        <p>Rating: {movieObj.rating}</p>
-
-                        <button onClick={() => { deleteMovie(movieObj.id) }}>Delete this movie</button>
-                    </section>
+                    <MovieSummary
+                        movieDetails={movieObj}
+                        onDelete={deleteMovie}
+                    />
                 )
             })}
         </>
